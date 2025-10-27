a9f93c42090778df86bb02b23d19a62ca070d3e5
Spencer <spencertout@gmail.com>
2025-10-27 19:39:29 +1100

    Commit 1: Plan
    Added a few comments to describe the steps I'm going to take to make this.

---
+++ textadventure.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Make 5 room, with the second leading to number 3 and 4, and room 3 leading to 5
+    // Player has name, health, current room and inventory
+    // Player can check inventory and stats
+    // Use items in inventory
+    // Save game after every move
+    // Add more later
+}
